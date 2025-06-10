afc909e47db3abda85a3261b6a81d4a9d823d88a
Mohd Rahmath <rahmathmohd1654@gmail.com>
2025-06-10 20:47:22 +0530

    all changes are done

---
+++ home/index.js
@@ -0,0 +1,22 @@
+let page=0;
+function createNewPage(){
+    page+=1;
+    localStorage.setItem(page,page);
+    console.log("the button is clicked");
+    showPages();
+}
+function showPages(){
+    const x =document.getElementById("unlist");
+    x.innerHTML="";
+    for(let i=1;i<=localStorage.length;i++){
+        const li =document.createElement('button');
+        const br =document.createElement('br');
+        li.textContent=`page ${localStorage.getItem(i,i)}`;
+        li.onclick=function(){
+          window.location.href = `/pagef/Page.html?page=${i}`;
+        }
+        li.appendChild(br);
+        x.appendChild(li);
+}
+}
+showPages();
