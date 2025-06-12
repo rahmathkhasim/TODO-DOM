da3116c676c4873c7383de26f0c61db70c6cbf77
Mohd Rahmath <rahmathmohd1654@gmail.com>
2025-06-12 14:03:38 +0530

    small fix of page counter

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
-let page=0;
 function createNewPage(){
+    let page = Number(localStorage.getItem('pageCount'))||0;
     page+=1;
     localStorage.setItem(page,page);
-    console.log("the button is clicked");
+    localStorage.setItem('pageCount',page);
     showPages();
 }
 function showPages(){
@@ -10,13 +10,13 @@ function showPages(){
     x.innerHTML="";
     for(let i=1;i<=localStorage.length;i++){
         const li =document.createElement('button');
-        const br =document.createElement('br');
         if(localStorage.getItem(i)!=null){
         li.textContent=`page ${localStorage.getItem(i,i)}`;
         li.onclick=function(){
           window.location.href = `pagef/Page.html?page=${i}`;
         }
-        li.appendChild(br);
+        li.style.marginBottom='6px';
+        li.style.display='block';
         x.appendChild(li);
       }
 }
